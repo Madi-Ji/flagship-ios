444ec9d4e96ce8a1cfcbe93dfe8cd881180dafae
Julien Madiot <Madi-Ji@users.noreply.github.com>
2020-11-04 20:03:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,17 +8,23 @@ let package = Package(
     
     platforms: [
         .iOS(.v8),
+        .macOS(.v10_14),
+        .tvOS(.v9)
     ],
     
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Flagship",
-            targets: ["Flagship"]),
+            targets: ["Flagship"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Flagship",path:"Source")
+        .target(
+            name: "Flagship",
+            path:"Source"
+        )
     ]
 )
